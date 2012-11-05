2616901d01342e0d46a3daf7d512f26621e15f67
timboe <timboe@gmail.com>
2012-11-05 19:12:06 +0000

    fix

--- src/com/timboe/rpsrts/enumerators/Pass.java
+++ src/com/timboe/rpsrts/enumerators/Pass.java
@@ -2,21 +2,7 @@ package com.timboe.rpsrts.enumerators;
 
 public class Pass {
 	public static String GetPass() {
-		return s("1PxE5B7SE4EsEwG");
+		return "";
 	}
-	
-	private static String s(String s){
-		String ss = "";
-        for (int S = 0; S < s.length(); ++S) {
-            char c = s.charAt(S);
-            if       (c >= 'a' && c <= 'm') c += 13;
-            else if  (c >= 'A' && c <= 'M') c += 13;
-            else if  (c >= 'n' && c <= 'z') c -= 13;
-            else if  (c >= 'N' && c <= 'Z') c -= 13;
-            else if  (c >= '5' && c <= '9') c -= 5;
-            else if  (c >= '0' && c <= '4') c += 5;
-            ss += c;
-        }
-        return ss;
-	}
+
 }
